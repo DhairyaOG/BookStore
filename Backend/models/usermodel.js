faa431c0a3a5c896a5f24ce77fb87eb8c081686d
DhairyaOG <aroradhairya314@gmail.com>
2025-06-26 01:42:57 +0530

    User Backend

--- Backend/models/usermodel.js
+++ Backend/models/usermodel.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String, 
+  password: String,
   googleId: String, 
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
